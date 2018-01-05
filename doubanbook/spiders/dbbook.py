55e5a86568c29fcb2714a6990f11f1eab4f315d1
unowmore <714745817@qq.com>
2018-01-05 16:32:14 +0800

    提交信息

---
+++ doubanbook/spiders/dbbook.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+from doubanbook.items import DoubanbookItem
+
+
+
+class DbbookSpider(scrapy.Spider):
+    name = "dbbook"
+    #allowed_domains = ["https://www.douban.com/doulist/1264675/"]
+    start_urls = ('https://www.douban.com/doulist/1264675/', )
+    URL = 'https://www.douban.com/doulist/1264675/?start=PAGE&sort=seq&sub_type='
+    def parse(self, response):
+        #print response.body
+
+        item = DoubanbookItem()
+        selector = scrapy.Selector(response)
+        books = selector.xpath('//div[@class="bd doulist-subject"]')
+        for each in books:
+
+            title = each.xpath('div[@class="title"]/a/text()').extract()[0]
+            rate = each.xpath('div[@class="rating"]/span[@class="rating_nums"]/text()').extract()[0]
+            people = each.xpath('div[@class="rating"]/span/text()').extract()[1]
+            author = re.search('<div class="abstract">(.*?)<br',each.extract(),re.S).group(1)
+            title = title.replace(' ','').replace('\n','')
+            author = author.replace(' ','').replace('\n','')
+            rate = rate.replace(' ', '').replace('\n', '')
+            item['title'] = title
+            item['rate'] = rate
+            item['author'] = author
+            item['people'] = people
+            # print 'title:' + title
+            # print 'rate:' + rate
+            # print author
+            # print ''
+            yield item
+            nextPage = selector.xpath('//span[@class="next"]/link/@href').extract()
+            if nextPage:
+                next = nextPage[0]
+                print next
+                yield scrapy.http.Request(next,callback=self.parse)
+
